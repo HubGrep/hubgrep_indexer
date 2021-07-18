12872634fbbc3e381e5c8a28bace8a2527f26de3
Henrik Eliasson <he@datenfreunde.com>
2021-07-18 18:40:06 +0200

    circleci config update

--- hubgrep_indexer/config/testing.py
+++ hubgrep_indexer/config/testing.py
@@ -5,7 +5,7 @@ from hubgrep_indexer.config import Config
 class TestingConfig(Config):
     TESTING = True
     REDIS_URL = 'redis://redis/1'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("HUBGREP_TEST_SQLALCHEMY_DATABASE_URI", False)
+    SQLALCHEMY_DATABASE_URI = "postgresql://hubgrep:hubgrep@test_postgres:5432/hubgrep"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SERVER_NAME = 'testserver'
