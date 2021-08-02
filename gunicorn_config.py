7b73467d25d82a264b49ffca074aacea254b90d1
jan <jan@kwoh.de>
2021-08-02 15:39:40 +0200

    remove prometheus gunicorn config

--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-# an extension targeted at Gunicorn deployments with an internal metrics endpoint
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
-
-
-# then in the Gunicorn config file:
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
-
-def child_exit(server, worker):
-    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
