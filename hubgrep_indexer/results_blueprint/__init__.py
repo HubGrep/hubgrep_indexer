24b6e48ab4b5223003f649fd622391dc743a3638
jan <jan@kwoh.de>
2021-08-04 21:01:25 +0200

    results path from config

--- hubgrep_indexer/results_blueprint/__init__.py
+++ hubgrep_indexer/results_blueprint/__init__.py
@@ -1,11 +1,11 @@
-import os
 from flask import Blueprint
 from flask import send_from_directory
+
+from flask import current_app
 
 results_bp = Blueprint("results", __name__, url_prefix="/results")
 
 
 @results_bp.route('/<path:path>')
 def serve_files(path):
-    results_path = os.environ.get('HUBGREP_RESULTS_PATH')
-    return send_from_directory(results_path, path)
+    return send_from_directory(current_app.config['RESULTS_PATH'], path)
